36dec94070a86ae655d3e046c994facc6d43607b
Richardxiao123 <richard@amego.tw>
2025-11-25 15:42:15 +0800

    Update p5.cpp

--- src/p5.cpp
+++ src/p5.cpp
@@ -5,15 +5,15 @@ void hanoi(int n, int from, int to, int aux)
 {
     if (n == 1)
     {
-        cout << "Move ring " << n << " from " << from << " to " << to << endl;
-        cout << "Move ring " << n << " from " << to << " to " << aux << endl;
+        cout << "Move disk " << n << " from " << from << " to " << to << endl;
+        cout << "Move disk " << n << " from " << to << " to " << aux << endl;
     }
     else
     {
         hanoi(n - 1, from, to, aux);
-        cout << "Move ring " << n << " from " << from << " to " << to << endl;
+        cout << "Move disk " << n << " from " << from << " to " << to << endl;
         hanoi(n - 1, aux, to, from);
-        cout << "Move ring " << n << " from " << to << " to " << aux << endl;
+        cout << "Move disk " << n << " from " << to << " to " << aux << endl;
         hanoi(n - 1, from, to, aux);
     }
 }
