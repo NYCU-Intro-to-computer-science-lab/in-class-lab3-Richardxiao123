3dd18a6037e32b9973b85b6b743079496b68d57b
Richardxiao123 <richard@amego.tw>
2025-11-25 13:37:30 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -3,8 +3,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    
-    
+long long factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+        return factorial(n - 1) * n;
+}
+
+long long sum(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+        return sum(n - 1) + n;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n == 0)
+    {
+        cout << 1 << endl;
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << factorial(n) << endl;
+        cout << sum(n) << endl;
+    }
     return 0;
 }
