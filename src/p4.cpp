017de4eb45a7db428efc69e24d319c1224fb5b59
Richardxiao123 <richard@amego.tw>
2025-11-25 15:28:02 +0800

    Update p4.cpp

--- src/p4.cpp
+++ src/p4.cpp
@@ -1,44 +1,51 @@
 #include <iostream>
 using namespace std;
 
-void printIndent(int depth) {
-    for (int i = 0; i < depth; i++) {
+void print(int n)
+{
+    if (n > 0)
+    {
         cout << "|--";
+        print(n - 1);
     }
 }
 
-int fib(int n, int depth)
+int fib(int n, int c)
 {
-    printIndent(depth);
-    cout << "SEARCH fib(" << n << ")\n";
-
-    if (n == 1 || n == 2) {
-        printIndent(depth);
-        cout << "GET fib(" << n << ") = 1\n";
+    if (n == 2)
+    {
+        return 1;
+    }
+    else if (n == 1)
+    {
         return 1;
     }
 
-    int a = fib(n - 1, depth + 1);
+    else
+    {
+        print(c + 1);
+        cout << "SEARCH fib(" << n - 1 << ")\n";
+        int x = fib(n - 1, c + 1);
+        print(c + 1);
+        cout << "GET fib(" << n - 1 << ") = " << x << endl;
 
-    printIndent(depth);
-    cout << "GET fib(" << n - 1 << ") = " << a << "\n";
-
-    int b = fib(n - 2, depth + 1);
-
-    printIndent(depth);
-    cout << "GET fib(" << n - 2 << ") = " << b << "\n";
-
-    printIndent(depth);
-    cout << "GET fib(" << n << ") = " << a + b << "\n";
-
-    return a + b;
+        print(c + 1);
+        cout << "SEARCH fib(" << n - 2 << ")\n";
+        int y = fib(n - 2, c + 1);
+        print(c + 1);
+        cout << "GET fib(" << n - 2 << ") = " << y << endl;
+        return x + y;
+    }
 }
 
 int main()
 {
     int n;
     cin >> n;
-    cout << fib(n, 0) << endl;
-    return 0;
+    int c = 0;
+    cout << "SEARCH fib(" << n << ")\n";
+    int a = fib(n, c);
+    cout << "GET fib(" << n << ") = " << a << endl;
+    cout << a << endl;
 }
 
